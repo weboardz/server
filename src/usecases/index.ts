a8c5a1f998979bca759b6d083d5948b1c036b6d6
samuel <shemlouk@gmail.com>
2023-06-21 16:54:04 -0300

    chore: update index file from use cases

--- src/usecases/index.ts
+++ src/usecases/index.ts
@@ -1,4 +1,6 @@
 export * from "./log-user";
 export * from "./log-user/types";
+export * from "./register-board";
+export * from "./register-board/types";
 export * from "./register-user";
 export * from "./register-user/types";
